fbfe44a2813351bcf09552c4c552dbf395cd5e0b
cmdeveloped <cmwebdevs@gmail.com>
2018-04-28 20:14:41 -0500

    Add initial directory and bookshelf components and set up routing.

---
+++ src/components/AllBooks.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class AllBooks extends Component {
+  render() {
+    return (
+      <div>
+        AllBooks
+      </div>
+    )
+  }
+}
+
+export default AllBooks
---
+++ src/components/BookShelves.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class BookShelves extends Component {
+  render() {
+    return (
+      <div>
+        BookShelves
+      </div>
+    )
+  }
+}
+
+export default BookShelves
