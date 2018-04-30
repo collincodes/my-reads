b7e3f3fda4d5da57249e19a134f0468fd53b1ebd
cmdeveloped <cmwebdevs@gmail.com>
2018-04-29 22:33:44 -0500

    Include more styling and add sorting feature in book directory

--- src/components/AllBooks.js
+++ src/components/AllBooks.js
@@ -5,35 +5,61 @@ import sortBy from 'sort-by'
 
 class AllBooks extends Component {
   // define proptypes here
+  static propTypes = {
+    contacts: PropTypes.array.isRequired
+  }
 
   // set initial state for filter query
+  state = {
+    query: ''
+  }
 
   // function to update the state for filter
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+  }
 
   // function to reset the state
+  clearQuery = () => {
+    this.setState({ query: '' })
+  }
 
   render() {
     const { books } = this.props
+    const { query } = this.state
 
     // initialize our showingBooks query
+    let showingBooks
+    if (query) {
+      const match = new RegExp(escapeRegExp(query), 'i')
+      showingBooks = books.filter((book) => match.test(book.title))
+    } else {
+      showingBooks = books
+    }
 
-    // after we filter the books by query, sort them by name
+    // after we filter the books by query, sort them by title
+    showingBooks.sort(sortBy('title'))
 
     return (
       <div className='all-books'>
         <div className='search-query'>
-          <input name='search' type='text' placeholder='Search Books' />
+          <input
+            className='search-books'
+            name='search'
+            type='text'
+            placeholder='Search Books'
+            value={query}
+            onChange={(e) => this.updateQuery(e.target.value)}
+          />
         </div>
 
         <ol className='book-list'>
-          {books.map((book, index) => (
+          {showingBooks.map((book, index) => (
             <li key={book.id} className='book'>
-              <div className='book-cover' style={{
-                backgroundImage: `url(${book.imageLinks.thumbnail})`
-              }}/>
-              <h1>{book.title}</h1>
-              <h6>{book.subtitle}</h6>
-              <h6>Written By: {book.authors[0]}</h6>
+              <img className='book-cover' src={book.imageLinks.thumbnail}/>
+              <h1 className='book-title'>{book.title}</h1>
+              <h6 className='book-subtitle'>{book.subtitle}</h6>
+              <h6 className='book-author'>{book.authors[0]}</h6>
             </li>
           ))}
         </ol>
